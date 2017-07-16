6f3fd25ca1d0814d7820917ef1dd381e393e9b0c
Thakur Nishant Singh <nishantbhb@gmail.com>
2017-07-16 06:37:08 +0530

    Final commit backend

---
+++ Swiggy-API/src/main/java/com/swiggy/model/Status.java
@@ -0,0 +1,23 @@
+package com.swiggy.model;
+
+public class Status {
+	String status;
+
+	public Status() {
+		super();
+	}
+
+	public Status(String status) {
+		super();
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
